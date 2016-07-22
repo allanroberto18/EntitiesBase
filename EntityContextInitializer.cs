dcd632a9cff982ebc6dec31a20bb1fdc2b47d249
Allan Roberto <allanroberto18@gmail.com>
2016-07-22 14:38:25 -0300

    coclusão da instalação

---
+++ EntityContextInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Entity;
+
+namespace Entities
+{
+    class EntityContextInitializer : CreateDatabaseIfNotExists<EntityContext>
+    {
+        protected override void Seed(EntityContext context)
+        {
+            Models.Usuarios master = new Models.Usuarios();
+            master.SetParams("Master", "master@smssim.com", "master1234", 1);
+
+            Models.Usuarios usuario = new Models.Usuarios();
+            usuario.SetParams("Usuário", "usuario@smssim.com", "resolve", 1);
+
+            Models.Mensagens mensagem = new Models.Mensagens();
+            mensagem.SetParams(1,"Feliz Aniversário",3,1);
+
+            context.Database.Connection.Open();
+
+            context.Usuarios.Add(master);
+            context.Usuarios.Add(usuario);
+            context.Mensagens.Add(mensagem);
+
+            context.SaveChanges();
+
+            context.Database.Connection.Close();
+
+            base.Seed(context);
+        }
+    }
+}
---
+++ Models/SimsMensagens.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    [Table("sims_mensagens")]
+    public class SimsMensagens
+    {
+        public int Id { get; set; }
+        public int SimId { get; set; }
+        public int Disparo { get; set; }
+        public DateTime DataDisparo { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public int Status { get; set; }
+
+        public void SetParams(int simId, int disparo, int status)
+        {
+            SimId = simId;
+            Disparo = disparo;
+            DataDisparo = DateTime.Today;
+            Status = status;
+        }
+
+        public void SetParams(int id, int simId, int disparo, int status)
+        {
+            Id = id;
+
+            SetParams(simId, disparo, status);
+        }
+    }
+}
